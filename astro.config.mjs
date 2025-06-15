6ddcc823ba46efcd2e807534c0b829ea38cb22b1
rshivaganesh <rshivaganesh@gmail.com>
2025-06-15 22:24:29 +0530

    astro config changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,11 @@ import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 
 import sitemap from "@astrojs/sitemap";
+import { SITE } from './src/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://foxi.netlify.app/",
+  site: SITE.website,
   integrations: [
     tailwind(),
     icon(),
@@ -18,4 +19,22 @@ export default defineConfig({
       },
     }),
   ],
+  output: 'static',
+  base: '/',
+  trailingSlash: 'always',
+  build: {
+    format: 'directory'
+  },
+  vite: {
+    server: {
+      host: true
+    }
+  },
+  experimental: {
+    redirects: true
+  },
+  redirects: {
+    'events.rsglab.online': '/events',
+    'events.rsglab.online/*': '/events/:splat'
+  }
 });
